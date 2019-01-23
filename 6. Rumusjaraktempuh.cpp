1ff58b2a6d5adbd49e6d68f35ed053b185554465
DanuAdiP <danu261216@gmail.com>
2019-01-23 21:24:11 +0700

    Commit Pertama

---
+++ 6. Rumusjaraktempuh.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float S,v,t;
+	cout<<"MENGHITUNG JARAK TEMPUH"<<endl<<endl;
+	cout<<"Rumus S = v*t"<<endl;
+	cout<<"Masukkan Kecepatan (v) = ";cin>>v;
+	cout<<"Masukkan Waktu (t)     = ";cin>>t;
+	S=v*t;
+	cout<<"Maka Jarak tempuh (S)  = "<<S;
+}
